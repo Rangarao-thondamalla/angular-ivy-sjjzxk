c9a479df70efa246c09a8e6265d4a9446890c7e0
Rangarao-thondamalla <t.rangarao007@gmail.com>
2021-07-20 18:39:55 +0530

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HomeComponent } from './home/home.component';
 import { EmployeeComponent } from './home/employee/employee.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule , ReactiveFormsModule ],
   declarations: [ AppComponent, HelloComponent, HomeComponent, EmployeeComponent],
   bootstrap:    [ AppComponent ]
 })
--- src/app/home/employee/employee.component.ts
+++ src/app/home/employee/employee.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit , Output, EventEmitter} from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-employee',
@@ -6,4 +8,19 @@ import { Component, OnInit , Output, EventEmitter} from '@angular/core';
   styleUrls: ['./employee.component.css']
 })
 export class EmployeeComponent {
+
+  regForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    pwd: new FormControl(''),
+    comment : new FormControl(''),
+    gender : new FormControl(''),
+    imgfile : new FormControl(''),
+    remberme : new FormControl('')
+  });
+
+  onSubmit() {
+  // TODO: Use EventEmitter with form value
+  console.warn(this.regForm.value);
+  }
 }
