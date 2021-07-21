f02f544773aee43630c92482726d4af8f9847dc8
Rangarao-thondamalla <t.rangarao007@gmail.com>
2021-07-21 10:29:47 +0530

    3 files modified

--- src/app/home/employee/employee.component.ts
+++ src/app/home/employee/employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit , Output, EventEmitter} from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators ,ValidationErrors} from '@angular/forms';
 
 
 @Component({
@@ -10,17 +10,25 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class EmployeeComponent {
 
   regForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    pwd: new FormControl(''),
-    comment : new FormControl(''),
-    gender : new FormControl(''),
-    imgfile : new FormControl(''),
-    remberme : new FormControl('')
+    name: new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(10)]),
+    email: new FormControl('',[Validators.required,Validators.email]),
+    pwd: new FormControl('',Validators.required),
+    comment : new FormControl('',Validators.required),
+    gender : new FormControl('', Validators.required),
+    imgfile : new FormControl('', Validators.required),
+    remberme : new FormControl('', Validators.required)
   });
+
+  get email(){ return this.regForm.get('email')};
+  get name(){ return this.regForm.get('name')};
+  get pwd() { return this.regForm.get('pwd')};
+  get comment() { return this.regForm.get('comment')};
+  get imgfile() { return this.regForm.get('imgfile')};
+
+
 
   onSubmit() {
   // TODO: Use EventEmitter with form value
-  console.warn(this.regForm.value);
+  console.warn(this.regForm.value); 
   }
 }
