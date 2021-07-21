b1b3e0f526e09beed94db970ad7d7ad40d5be189
Rangarao-thondamalla <t.rangarao007@gmail.com>
2021-07-21 12:59:18 +0530

    3 files modified

--- src/app/home/employee/employee.component.ts
+++ src/app/home/employee/employee.component.ts
@@ -10,10 +10,10 @@ import { FormGroup, FormControl, Validators ,ValidationErrors} from '@angular/fo
 export class EmployeeComponent {
 
   regForm = new FormGroup({
-    name: new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(10)]),
-    email: new FormControl('',[Validators.required,Validators.email]),
-    pwd: new FormControl('',Validators.required),
-    comment : new FormControl('',Validators.required),
+    name: new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(10),Validators.pattern("[a-zA-Z][a-zA-Z ]{2,}")]),
+    email: new FormControl('',[Validators.required,Validators.email,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]),
+    pwd: new FormControl('',[Validators.required,Validators.pattern("^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$")]),
+    comment : new FormControl('',[Validators.required,Validators.minLength(10)]),
     gender : new FormControl('', Validators.required),
     imgfile : new FormControl('', Validators.required),
     remberme : new FormControl('', Validators.required)
