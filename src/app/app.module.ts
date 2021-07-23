c5e6ee4926e45486f9904d22af1d326c4b34a84e
Rangarao-thondamalla <t.rangarao007@gmail.com>
2021-07-23 12:55:48 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router'; // CLI imports router
 import { ReactiveFormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -15,7 +16,7 @@ import { ApiService } from './shared/api.service';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule , ReactiveFormsModule , AppRoutingModule, RouterModule, CommonModule, HttpClientModule],
+  imports:      [ BrowserModule, FormsModule , ReactiveFormsModule , AppRoutingModule, RouterModule, CommonModule, HttpClientModule , NgxPaginationModule],
   declarations: [ AppComponent, HomeComponent, EmployeeComponent],
   bootstrap:    [ AppComponent ],
   providers: [ ApiService ],
