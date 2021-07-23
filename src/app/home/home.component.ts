de6927304494b09561837265ca2990d7958d567a
Rangarao-thondamalla <t.rangarao007@gmail.com>
2021-07-23 12:53:10 +0530

    2 files modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, Output ,EventEmitter } from '@angular/core';
 
 import { ApiService } from '../shared/api.service';
 import { Employee } from './employee/employee';
+import { Router } from '@angular/router';
 
 @Component({ 
   selector: 'app-home',
@@ -10,9 +11,12 @@ import { Employee } from './employee/employee';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private api : ApiService ){}
+  constructor(private api : ApiService, private router: Router ){}
   employees : any [];
 
+  firstName :any ;
+
+  p:any;
   ngOnInit(){ this.getallEmployees();}
   getallEmployees(){
      this.api.getEmployee()
@@ -28,4 +32,23 @@ export class HomeComponent implements OnInit {
       this.getallEmployees();
     })
   }
+
+  Search(){
+    if(this.firstName == ""){
+      this.ngOnInit();
+    } else{
+      this.employees = this.employees.filter( res=> {
+        return res.name.toLocaleLowerCase().match(this.firstName.toLocaleLowerCase());
+      })
+    }
+  }
+
+  key : string = 'id';
+  reverse : boolean = false;
+  sort(key){
+    this.key = key;
+    this.reverse = !this.reverse;
+  }
+
+
 }
